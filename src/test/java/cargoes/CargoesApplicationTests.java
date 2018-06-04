38be0b1e35f66af53ce537cc7e2ea1a01c71451a
lianghg <1273890737@qq.com>
2018-06-04 20:37:21 +0800

    -

--- src/test/java/cargoes/CargoesApplicationTests.java
+++ src/test/java/cargoes/CargoesApplicationTests.java
@@ -2,15 +2,39 @@ package cargoes;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CargoesApplicationTests {
 
+	@Autowired
+	private RedisTemplate<Object, Object> redisTemplate;
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
 	@Test
 	public void contextLoads() {
+
+		ValueOperations<String, String> svalue = stringRedisTemplate.opsForValue();
+		svalue.set("test", System.currentTimeMillis()+"-");
+		
+	}
+	
+	@Test
+	public void contextLoads2() {
+
+
+		ValueOperations<String, String> svalue = stringRedisTemplate.opsForValue();
+		String text = svalue.get("test");
+		System.out.println(text);
+		
 	}
 
 }
