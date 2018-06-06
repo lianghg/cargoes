54f856b06944b0d7ff6a627affffb32d62af3827
lianghg <1273890737@qq.com>
2018-06-07 00:29:18 +0800

    -

---
+++ src/main/java/cargoes/model/vo/DataEntity.java
@@ -0,0 +1,77 @@
+package cargoes.model.vo;
+
+import java.util.Date;
+
+public class DataEntity<T> {
+
+	private T result;
+
+	private String message;
+
+	private int status;
+
+	private boolean flag;
+
+	private String exception;
+
+	private String error;
+
+	private Date timestamp;
+
+	public T getResult() {
+		return result;
+	}
+
+	public void setResult(T result) {
+		this.result = result;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public boolean isFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+
+	public String getException() {
+		return exception;
+	}
+
+	public void setException(String exception) {
+		this.exception = exception;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
